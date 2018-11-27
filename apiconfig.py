59ed5215a0c8b56db07b781f678a7fef0317c9ce
SakuraSa233 <sakura.sa233@gmail.com>
2018-11-27 13:54:09 +0800

    optimize: remove unused variables and dependencies

--- apiconfig.py
+++ apiconfig.py
@@ -14,11 +14,9 @@ MU_REGEX = '%5m%id.%suffix'
 SERVER_PUB_ADDR = '127.0.0.1'  # mujson_mgr need this to generate ssr link
 API_INTERFACE = 'modwebapi'  # glzjinmod, modwebapi
 
+# Mod webapi
 WEBAPI_URL = 'https://zhaoj.in'
 WEBAPI_TOKEN = 'glzjin'
-
-# mudb
-MUDB_FILE = 'mudb.json'
 
 # Mysql
 MYSQL_HOST = '127.0.0.1'
@@ -32,12 +30,6 @@ MYSQL_SSL_CA = ''
 MYSQL_SSL_CERT = ''
 MYSQL_SSL_KEY = ''
 
-# API
-API_HOST = '127.0.0.1'
-API_PORT = 80
-API_PATH = '/mu/v2/'
-API_TOKEN = 'abcdef'
-API_UPDATE_TIME = 60
 
 # Manager (ignore this)
 MANAGE_PASS = 'ss233333333'
